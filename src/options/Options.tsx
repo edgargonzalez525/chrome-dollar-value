94647ef38dcb93f70bf7dc10fb6f65d009aa427c
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2019-02-18 13:13:32 -0300

    feat: add param showNotificaitons

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -1,7 +1,9 @@
-import * as React from 'react';
 import './Options.scss';
-import {Country} from '../model/country';
-import {CountryService} from '../services/country.service';
+
+import * as React from 'react';
+
+import { Country } from '../model/country';
+import { CountryService } from '../services/country.service';
 
 interface AppProps {
 }
@@ -11,6 +13,13 @@ interface AppState {
   country: Country;
   countries: Country[];
   loading: boolean;
+  showNotifications: boolean;
+}
+
+interface IResultChromeStorage {
+  refreshMinutes: number;
+  country: string;
+  showNotifications: boolean;
 }
 
 export default class Options extends React.Component<AppProps, AppState> {
@@ -19,6 +28,7 @@ export default class Options extends React.Component<AppProps, AppState> {
     country: null,
     countries: [],
     loading: false,
+    showNotifications: true
   };
 
   constructor(props: AppProps) {
@@ -27,23 +37,18 @@ export default class Options extends React.Component<AppProps, AppState> {
   }
 
   componentDidMount() {
-    let self = this;
     CountryService.getCountries()
     .subscribe((countries: Country[]) => {
-      chrome.storage.sync.get(['refreshMinutes', 'country'], function (result: { refreshMinutes: number, country: string }) {
-        const refreshMinutes = result.refreshMinutes || 30;
-        const selectedCountry: Country = result.country ?
-          JSON.parse(result.country) :
+      chrome.storage.sync.get(['refreshMinutes', 'country', 'showNotifications'], ({ refreshMinutes = 30, country, showNotifications = true }: IResultChromeStorage) => {
+        const selectedCountry: Country = country ?
+          JSON.parse(country) :
           CountryService.DEFAULT_COUNTRY;
-        console.log({
+
+        this.setState({
           refreshMinutes: refreshMinutes.toString(),
           country: selectedCountry,
           countries: countries,
-        });
-        self.setState({
-          refreshMinutes: refreshMinutes.toString(),
-          country: selectedCountry,
-          countries: countries,
+          showNotifications
         });
       });
     });
@@ -52,17 +57,20 @@ export default class Options extends React.Component<AppProps, AppState> {
 
   onSubmit($event) {
     $event.preventDefault();
-    const self = this;
-    self.setState({loading: true});
+    this.setState({ loading: true });
+
+    const { refreshMinutes, country, showNotifications } = this.state;
     chrome.storage.sync.set({
-      refreshMinutes: parseInt(this.state.refreshMinutes),
-      country: JSON.stringify(this.state.country),
-    }, function () {
-      self.setState({loading: false});
+      refreshMinutes: parseInt(refreshMinutes, 10),
+      country: JSON.stringify(country),
+      showNotifications
+    }, () => {
+      this.setState({ loading: false });
     });
   }
 
   render() {
+    const { country, countries, refreshMinutes, showNotifications, loading } = this.state;
     return (
       <div>
         <nav className="navbar navbar-light bg-light">
@@ -83,7 +91,7 @@ export default class Options extends React.Component<AppProps, AppState> {
               </label>
               <div className="col-sm-8">
                 <select id="inputState" className="form-control"
-                        value={this.state.country ? this.state.country.alpha2Code : ''}
+                        value={country ? country.alpha2Code : ''}
                         onChange={($event) => {
                           const alphaCode = $event.target.value;
                           this.setState((state) => {
@@ -92,7 +100,7 @@ export default class Options extends React.Component<AppProps, AppState> {
                             };
                           });
                         }}>
-                  {this.state.countries.map((country, i) => {
+                  {countries.map((country, i) => {
                     return <option key={i} value={country.alpha2Code}>{country.name}</option>;
                   })}
                 </select>
@@ -105,14 +113,27 @@ export default class Options extends React.Component<AppProps, AppState> {
               <div className="col-sm-8">
                 <input type="number" className="form-control" id="inputEmail3"
                        required={true}
-                       value={this.state.refreshMinutes}
+                       value={refreshMinutes}
                        onChange={($event) => this.setState({refreshMinutes: $event.target.value || ''})}/>
+              </div>
+            </div>
+            <div className="form-group row">
+              <label htmlFor="showNotifications" className="col-sm-4 col-form-label">
+                Show notifications for rate change
+              </label>
+              <div className="col-sm-8">
+                <select id="showNotifications" className="form-control"
+                        value={showNotifications ? 'true' : ''}
+                        onChange={($event) => this.setState({ showNotifications: Boolean($event.target.value) })}>
+                  <option value={'true'}>Yes</option>
+                  <option value={''}>No</option>
+                </select>
               </div>
             </div>
             <div className="form-group row">
               <div className="offset-md-4 col-sm-8">
                 <button type="submit" className="btn btn-primary"
-                        disabled={this.state.loading}>
+                        disabled={loading}>
                   Save
                 </button>
               </div>
